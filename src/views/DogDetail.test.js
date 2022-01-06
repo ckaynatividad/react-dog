cb43b4d72eac3a35a322a6912108049aef777c9c
ckaynatividad <natividad.ckay@gmail.com>
2022-01-05 20:14:49 -0800

    added keys and admin files

--- src/views/DogDetail.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Dog from './DogDetail';
-
-test('dog details rendering', () => {
-  const container = render(
-    <Dog match={{ params: { id: 1 } }} />
-  );
-  expect(container).toMatchSnapshot();
-});
